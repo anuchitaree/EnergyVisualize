0e0fcf0dcc6d4d802770588dbdb8b1c8b5b22fdc
diatanuchit <anuchit.aree.a9f@ap.denso.com>
2026-05-15 16:28:20 +0700

    first

---
+++ Models/EnergyMinute.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnergyVisualize.Models
+{
+    public class EnergyMinute
+    {
+        public int Id { get; set; }
+
+        public DateTime Minute { get; set; }
+        public float EnergyKwh { get; set; }
+        public float MaxPower { get; set; } // 🔥 peak ใน 1 นาที
+
+        public EnergyMinute(DateTime Minute, float EnergyKwh, float MaxPower) 
+        { 
+            this.Minute = Minute;
+            this.EnergyKwh = EnergyKwh;
+            this.MaxPower = MaxPower;
+        }
+    }
+}
